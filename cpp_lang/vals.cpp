436b5cc01d4e8edb59d545893943938330aa7fe6
Sandeep Chezhian A <sandeepchezhian@gmail.com>
2025-04-26 20:53:09 +0530

    Largest element problem solved'

--- cpp_lang/vals.cpp
+++ cpp_lang/vals.cpp
@@ -3,12 +3,35 @@
 
 using namespace std;
 
+void swapNum(int a, int b)
+{
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+void swapNumRef(int &a, int &b)
+{
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
 int main()
 {
     int x = 15;
+    int y = 17;
     cout << "The value of x at " << &x << " is " << x << endl;
     x++;
     cout << "The value of x at " << &x << " is " << x << endl;
+
+    // swapNum(x, y); - Does not have an impact on the values of the variables
+    // because the arguments are passed by value and not by reference.
+
+    swapNumRef(x, y);
+
+    cout << "The value of x is : " << x << endl;
+    cout << "The value of y is : " << y << endl;
 
     // The address of the variable x does not change but the value
     // of the variable has been incremented at the same memory location.
---
+++ data_structures/arrays/easy_problems/largestElement.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int largestElement(vector<int> &x);
+
+int main()
+{
+    vector<int> data1 = {1, 2, 3, 4, 392, 292, 29};
+    int max = largestElement(data1);
+
+    cout << "The largest element in the array is : " << max << endl;
+    cout << "The time complexity of this approach is O(N) and the space complexity is O(1). ";
+    return 0;
+}
+
+int largestElement(vector<int> &x)
+{
+    int max = x[0];
+    for (int num : x)
+    {
+        if (num > max)
+        {
+            max = num;
+        }
+    }
+
+    return max;
+}
