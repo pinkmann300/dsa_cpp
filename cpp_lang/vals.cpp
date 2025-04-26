03ee05231a198ddcfd8309b142b579abb45b26cc
Sandeep Chezhian A <sandeepchezhian@gmail.com>
2025-04-26 15:07:57 +0530

    Pass by value

--- cpp_lang/vals.cpp
+++ cpp_lang/vals.cpp
@@ -5,5 +5,13 @@ using namespace std;
 
 int main()
 {
+    int x = 15;
+    cout << "The value of x at " << &x << " is " << x << endl;
+    x++;
+    cout << "The value of x at " << &x << " is " << x << endl;
+
+    // The address of the variable x does not change but the value
+    // of the variable has been incremented at the same memory location.
+
     return 0;
 }
