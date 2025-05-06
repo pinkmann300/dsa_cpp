a363a4d49b3e622895919d7d80d9acbb589fadb6
Sandeep Chezhian A <sandeepchezhian@gmail.com>
2025-05-06 11:15:27 +0530

    Move zeros file added

---
+++ data_structures/arrays/easy_problems/moveZeros.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+void moveZeros(vector<int> &data1);
+
+int main()
+{
+    return 0;
+}
+
+void moveZeros(vector<int> &data1)
+{
+    // Compute the first position of the zero. 
+
+    
+}
