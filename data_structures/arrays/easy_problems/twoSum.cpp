cb4c526fa4a5f782bbd3ac4c2b9d9604246287a1
Sandeep Chezhian A <sandeepchezhian@gmail.com>
2025-05-07 20:55:35 +0530

    variant 2 of two sum added

--- data_structures/arrays/easy_problems/twoSum.cpp
+++ data_structures/arrays/easy_problems/twoSum.cpp
@@ -4,15 +4,22 @@
 using namespace std;
 
 bool twoSum(vector<int> &data1, int targetSum);
+vector<int> twoSumVar2(vector<int> &data1, int targetSum);
+void printArray(vector<int> &data1);
 
 int main()
 {
     vector<int> data1 = {2, 6, 5, 8, 11};
-    cout << (twoSum(data1, 15) ? "YES" : "NO") << endl;
+    vector<int> answer = twoSumVar2(data1, 15);
+    printArray(answer);
+
+    vector<int> answer2 = twoSumVar2(data1, 14);
+    printArray(answer2);
     return 0;
 }
 
 // Brute force method.
+// Variant 1
 bool twoSum(vector<int> &data1, int targetSum)
 {
     int length = data1.size(); // Length of the array.
@@ -32,4 +39,35 @@ bool twoSum(vector<int> &data1, int targetSum)
     }
 
     return false;
+}
+
+// Brute Force
+// Variant 2
+vector<int> twoSumVar2(vector<int> &data1, int targetSum)
+{
+    int length = data1.size();
+    vector<int> answer = {-1, -1};
+    for (int i = 0; i < length; i++)
+    {
+        int remainder = targetSum - data1[i];
+        for (int j = i + 1; j < length; j++)
+        {
+            if (data1[j] == remainder)
+            {
+                answer[0] = i;
+                answer[1] = j;
+            }
+        }
+    }
+    return answer;
+}
+
+void printArray(vector<int> &data1)
+{
+    for (int k : data1)
+    {
+        cout << k << ", ";
+    }
+
+    cout << "\n";
 }
