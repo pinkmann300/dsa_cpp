e2a82e3bc98729d5966f2fd3286db5abda199a8e
Sandeep Chezhian A <sandeepchezhian@gmail.com>
2025-05-17 23:19:44 +0530

    Buy and sell function added'

--- data_structures/arrays/medium_problems/buyAndSell.cpp
+++ data_structures/arrays/medium_problems/buyAndSell.cpp
@@ -3,6 +3,8 @@
 
 using namespace std;
 
+void sort012Optim(vector<int> &data1);
+
 int maxProfitBrute(vector<int> &data1)
 {
     int maxProfit = 0;
@@ -20,20 +22,51 @@ int maxProfitBrute(vector<int> &data1)
             }
         }
     }
-
     return maxProfit;
 }
 
+int maxProfit(vector<int> &data1)
+{
+    int minPrice = data1[0];
+    int maxProfit = 0;
 
-
- int maxProfit(vector<int> &data1)
- {
- }
+    for (int i = 0; i < data1.size(); i++)
+    {
+        minPrice = min(minPrice, data1[i]);
+        maxProfit = max(maxProfit, data1[i] - minPrice);
+    }
+    return maxProfit;
+}
 
 int main()
 {
     vector<int> buySellDates = {7, 1, 5, 3, 6, 4};
-    cout << maxProfitBrute(buySellDates) << endl;
-    cout << INT_MAX << endl;
+    cout << maxProfit(buySellDates) << endl;
     return -1;
+}
+
+void sort012Optimal(vector<int> &data1)
+{
+    int low = 0;
+    int mid = 0;
+    int high = data1.size() - 1;
+
+    while (mid <= high)
+    {
+        if (data1[mid] == 0)
+        {
+            swap(data1[low], data1[mid]);
+            low++;
+            mid++;
+        }
+        else if (data1[mid] == 1)
+        {
+            mid++;
+        }
+        else if (data1[mid] == 2)
+        {
+            swap(data1[mid], data1[high]);
+            high--;
+        }
+    }
 }
