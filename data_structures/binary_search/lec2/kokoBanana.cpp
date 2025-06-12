8f8e68ad2a27620ea40f185afbeb2e007e0b7e38
Sandeep Chezhian A <sandeepchezhian@gmail.com>
2025-06-12 09:15:00 +0530

    Added new files

---
+++ data_structures/binary_search/lec2/kokoBanana.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int kokoBanana(vector<int> &piles);
+
+int main()
+{
+    return 0;
+}
+
+// This will be the first problem solved using the monitor.
+// Super excited for this in some sense.
+
+int kokoBanana(vector<int> &piles)
+{
+    return 2;
+}
