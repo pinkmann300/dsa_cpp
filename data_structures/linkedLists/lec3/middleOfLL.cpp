a28653560b578887e4ce1a2b9ab93454320dedf4
Sandeep Chezhian A <sandeepchezhian@gmail.com>
2025-06-25 18:54:45 +0530

    Brute force method added

--- data_structures/linkedLists/lec3/middleOfLL.cpp
+++ data_structures/linkedLists/lec3/middleOfLL.cpp
@@ -25,68 +25,60 @@ public:
     }
 };
 
-int totalLength(Node *head)
+int lengthOfLinkedList(Node *head)
 {
     int count = 0;
-    while (head != nullptr)
+    Node *temp = head;
+
+    while (temp != nullptr)
     {
-        head = head->next;
+        temp = temp->next;
         count++;
     }
 
     return count;
 }
 
-int middleValue(Node *head)
+int middleElement(Node *head)
 {
-    int totalLength2 = totalLength(head);
-    int middlePos = (totalLength2 / 2) + 1;
-    int counter = 0;
-
-    while (head != nullptr)
+    int length = lengthOfLinkedList(head);
+    if (length == 0)
     {
-        counter++;
-        if (counter == middlePos)
-        {
-            return head->data;
-        }
-        else
-        {
-            head = head->next;
-        }
+        return 0;
     }
 
-    return -1;
-}
+    Node *tail = head;
+    int midPos = (length / 2) + 1;
 
-int midTH(Node *head)
-{
-    Node *fast = head;
-    Node *slow = head;
+    int tempCount = 0;
 
-    while (fast != nullptr)
+    while (tail != nullptr)
     {
-        fast = fast->next->next;
-        slow = slow->next;
+        tempCount++;
+        if (tempCount == midPos)
+        {
+            return tail->data;
+        }
+        tail = tail->next;
     }
 
-    return slow->data;
+    return 0;
 }
 
 int main()
 {
+    // Creating a sample linked list:
     Node *head = new Node(1);
     head->next = new Node(2);
     head->next->next = new Node(3);
     head->next->next->next = new Node(4);
     head->next->next->next->next = new Node(5);
-    head->next->next->next->next->next = new Node(6);
 
     // Find the middle node
-    int middleNode = midTH(head);
+    int middleValue = middleElement(head);
 
     // Display the value of the middle node
-    cout << "The middle node value is: " << middleNode << endl;
+    cout << "The middle node value is: " << middleValue << endl;
 
     return 0;
 }
