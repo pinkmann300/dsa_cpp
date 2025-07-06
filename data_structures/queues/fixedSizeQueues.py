c6a2b08a145619f365690f5f8cca3424e8e02d5c
Sandeep Chezhian A <sandeepchezhian@gmail.com>
2025-07-06 13:16:15 +0530

    Fixed size queues addeD

---
+++ data_structures/queues/fixedSizeQueues.py
@@ -0,0 +1,13 @@
+#The size of the queue is fixed. 
+
+class Queue: 
+    def __init__(self, size): 
+        self.size = size
+        self.queue = [] 
+        self.top = -1 
+        self.last = -1
+
+    def push(self, x): 
+        pass 
+         
+         
