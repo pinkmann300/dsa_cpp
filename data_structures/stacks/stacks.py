73d43221b736c8bcf8f9e89397cb21d1e0ba6cc7
Sandeep Chezhian A <sandeepchezhian@gmail.com>
2025-07-06 10:58:00 +0530

    Stacks in python

---
+++ data_structures/stacks/stacks.py
@@ -0,0 +1,14 @@
+#Simplest way to implement a stack in python is using lists. 
+
+stack1 = []
+stack1.append(10)
+stack1.append(20)
+stack1.append(30)
+
+
+print(stack1.pop())
+#Returns the top element in a stack and removes it from the list 
+#as well. 
+
+print(stack1)
+
