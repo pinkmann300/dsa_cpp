7ccb8a0d185b49d8baf45154e995cdf545ebb896
Sandeep Chezhian A <sandeepchezhian@gmail.com>
2025-07-20 00:44:12 +0530

    cleared spaces

--- data_structures/stacks/asteroidCollision.py
+++ data_structures/stacks/asteroidCollision.py
@@ -1,8 +1,9 @@
-# We are given an array asteroids of integers representing asteroids in a row. The indices of the asteriod in the array represent their relative position in space.
+# We are given an array asteroids of integers representing asteroids in a row. 
+# The indices of the asteriod in the array represent their relative position in space.
 # For each asteroid, the absolute value represents its size, and the sign represents its direction (positive meaning right, negative meaning left).
 # Each asteroid moves at the same speed.
 # Find out the state of the asteroids after all collisions. If two asteroids meet, the smaller one will explode. 
-#If both are the same size, both will explode. Two asteroids moving in the same direction will never meet.
+# If both are the same size, both will explode. Two asteroids moving in the same direction will never meet.
 
 def asteroidCollision(asteroids): 
     collisionStack = [] 
