a8d39318d92dabe41f24d54f94fb65c3fab920a4
Sandeep Chezhian A <sandeepchezhian@gmail.com>
2025-08-10 18:40:15 +0530

    Merge sort completed

--- data_structures/sorting/mergeSort.py
+++ data_structures/sorting/mergeSort.py
@@ -1,16 +1,37 @@
 def merge(list1, low, mid, high): 
-    pass 
+    temp = [] 
+    left = low 
+    right = mid + 1 
 
+    while (left <= mid and right <= high): 
+        if (list1[left] <= list1[right]): 
+            temp.append(list1[left]) 
+            left += 1 
+        else: 
+            temp.append(list1[right])
+            right += 1
+
+    while (left <= mid): 
+        temp.append(list1[left]) 
+        left += 1 
+    
+    while (right <= high): 
+        temp.append(list1[right])
+        right += 1
+
+    for i in range(low, high + 1): 
+        list1[i] = temp[i - low]
+    
 
 def mergeSort(list1, low, high): 
     if (low >= high):
         return 
     
-    mid = low + high // 2
+    mid = (low + high) // 2
     mergeSort(list1, low, mid) 
     mergeSort(list1, mid+1, high) 
-    merge(list1, low, mid, high) 
+    merge(list1, low, mid, high)
 
-
-
-print(3//2); 
+list1= [239,2923,102101,9201,292033,391] 
+mergeSort(list1, 0, len(list1) - 1) 
+print(list1)
