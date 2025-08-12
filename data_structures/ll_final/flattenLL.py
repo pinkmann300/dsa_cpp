f570f9890c06b5b214a658a54ae31a3bdbb1ecf1
Sandeep Chezhian A <sandeepchezhian@gmail.com>
2025-08-12 19:46:07 +0530

    Completed flattening linked list

--- data_structures/ll_final/flattenLL.py
+++ data_structures/ll_final/flattenLL.py
@@ -4,10 +4,8 @@ class Node:
         self.next = next 
         self.child = child 
 
-
 def flattenLL(head): 
     newList = [] 
-    
     temp = head 
     while (temp is not None): 
         temp2 = temp
@@ -15,7 +13,6 @@ def flattenLL(head):
             newList.append(temp2.data)
             temp2 = temp2.child 
         temp = temp.next 
-
 
     newList.sort() 
     dummyNode = Node(-1, None, None) 
@@ -26,26 +23,60 @@ def flattenLL(head):
     
     return dummyNode.child  
 
-
 def printLL(head): 
     while head is not None: 
         print(head.data, " -> ", end="")
         head = head.child 
 
+def merge2Heads(left, right):
+    dummyNode = Node(-1)
+    temp = dummyNode 
+
+    while (left is not None and right is not None): 
+        if (left.data <= right.data):
+            temp.child = left 
+            left = left.child
+        else: 
+            temp.child = right
+            right = right.child 
+
+        temp = temp.child
+
+    if (left is not None): 
+        temp.child = left 
+
+    if (right is not None): 
+        temp.child = right
+
+    temp.next = None
+    
+
+    return dummyNode.child 
+
+def flattenLinkedList(head):
+    if not head or not head.next:
+        return head
+
+    mergedHead = flattenLinkedList(head.next)
+    head = merge2Heads(head, mergedHead)
+    return head
+
+
 
 head = Node(5)
 head.child = Node(14)
-head.next = Node(10)
-head.next.child = Node(4)
-head.next.next = Node(12)
-head.next.next.child = Node(20)
-head.next.next.child.child = Node(13)
+head.next = Node(4)
+head.next.child = Node(10)
+head.next.next = Node(9)
+head.next.next.child = Node(13)
+head.next.next.child.child = Node(20)
 head.next.next.next = Node(7)
 head.next.next.next.child = Node(17) 
 
 
-head = flattenLL(head)
-printLL(head)
+printLL(flattenLinkedList(head))
+
+
     
 
     
