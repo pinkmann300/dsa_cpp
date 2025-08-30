eb93b7146aa3bfd720fa64036b47f8b8473e4f1d
Sandeep Chezhian A <sandeepchezhian@gmail.com>
2025-08-30 15:39:23 +0530

    Completed generation of subsets

--- data_structures/recursion/lec3.py
+++ data_structures/recursion/lec3.py
@@ -17,5 +17,47 @@ def removeAs(s, genVal):
         else: 
             return removeAs(s[1:], genVal + s[0])
         
-k = "saaandeep" 
-print(removeAs(k, ""))
+def subset(arr, genVal): 
+    if (len(arr) == 0): 
+        return [genVal] if genVal is not None else [] 
+    else: 
+        k = subset(arr[1:], genVal) 
+        r = subset(arr[1:], genVal + [arr[0]])
+
+        return k + r
+    
+
+def subsequences(s, genVal): 
+    if (len(s) == 0):
+        if genVal is not None:  
+            print(genVal, end=",")
+    else: 
+        k = subsequences(s[1:], genVal + s[0]) 
+        r = subsequences(s[1:], genVal) 
+
+        print(k, end=",") if k is not None else ""
+        print(r, end=",") if r is not None else ""
+
+def subsequencesSet(s, genVal): 
+    if (len(s) == 0): 
+        if genVal is not None: 
+            return [genVal] 
+    else: 
+        k = subsequencesSet(s[1:], genVal + s[0])
+        r = subsequencesSet(s[1:], genVal) 
+    
+        return k + r 
+    
+def generateSubsets(arr, genVal): 
+    if (len(arr) == 0): 
+        if genVal is not None or len(genVal) != 0:  
+            return [genVal] 
+    else: 
+        k = generateSubsets(arr[1:], genVal + [arr[0]]) 
+        r = generateSubsets(arr[1:], genVal)
+
+        return k + r
+    
+
+
+# Iterative functions begin here. 
