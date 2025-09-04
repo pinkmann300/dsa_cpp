6dd81ecfccb76b62cb0c5be3274c4d96b55e8c58
Sandeep Chezhian A <sandeepchezhian@gmail.com>
2025-09-05 00:04:33 +0530

    Completed robberies problem

---
+++ data_structures/dp/maxSumNon.py
@@ -0,0 +1,25 @@
+def maxSumNonAdj(arr): 
+    n = len(arr) 
+    dp = [-1] * n 
+    dp[0] = arr[0] 
+
+    for i in range(1,n): 
+        pick = arr[i] 
+
+        if i > 1: 
+            pick += dp[i - 2] 
+        nonPick = dp[i - 1]
+
+        dp[i] = max(pick, nonPick) 
+    return dp[n-1] 
+
+
+
+def robberies(arr): 
+    ans1 = maxSumNonAdj(arr[1:]) 
+    ans2 = maxSumNonAdj(arr[0:len(arr) - 1]) 
+    return max(ans1, ans2)
+
+
+arr = [2,3,2] 
+print(robberies(arr)) 
