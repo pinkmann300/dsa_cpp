0b4d8faa98cac351415705d2152edc9f7b661f65
Sandeep Chezhian A <sandeepchezhian@gmail.com>
2025-09-04 14:31:27 +0530

    Completed

--- data_structures/dp/frogJumpK.py
+++ data_structures/dp/frogJumpK.py
@@ -11,10 +11,27 @@ def frogJumpK(n, heights, k):
         dp[i] = minJump 
     return dp[n] 
 
+def frogJumpMemo(n, heights, k, dp): 
+    if n == 0:
+        return 0 
+    if dp[n] != -1: 
+        return dp[n] 
+    else: 
+        minJump = float('inf') 
+        for i in range(1, k + 1):
+            if (n - i) >= 0: 
+                jumps = frogJumpMemo(n - i, heights, k, dp) + abs(heights[n - i] - heights[n])
+                minJump = min(minJump, jumps) 
+        dp[n] = minJump
+        return dp[n] 
+
+
 height = [30,10,60,10,60,50] 
 n = 5 
 k = 2 
-l = frogJumpK(5, height, 2) 
+
+dp = [-1] * 6
+l = frogJumpMemo(5, height, 2, dp) 
 print("Frog jump: ", l) 
 
 
