1aa5b8fd6f3f7827445c55740eaba2ad8f1e3167
Sandeep Chezhian A <sandeepchezhian@gmail.com>
2025-09-06 17:04:36 +0530

    Completed memoized approach

--- data_structures/dp/lec2/ninjaTraining.py
+++ data_structures/dp/lec2/ninjaTraining.py
@@ -14,10 +14,30 @@ def ninjaTrainRec(trainingArr, days, last):
         return maxi
     
 
+def ninjaTrainMemoized(trainingArr, days, last, dp): 
+    if dp[days][last] != -1: 
+        return dp[days][last] 
+    if days == 0:
+        maximum = 0  
+        for i in range(0, 3):
+            if (i != last): 
+                maximum = max(trainingArr[days][i], maximum) 
+        dp[days][last] = maximum 
+        return dp[days][last] 
+    else: 
+        maxi = 0 
+        for k in range(0, 3):
+            activitySum = trainingArr[days][k] + ninjaTrainRec(trainingArr, days - 1, k) 
+            maxi = max(maxi, activitySum) 
+        dp[days][last] = maxi
+        return dp[days][last] 
+
 points = [[10, 40, 70],
               [20, 50, 80],
               [30, 60, 90]]
 
+dp = [[-1] * 4] * 3
+
 n = len(points)  # Get the number of days.
     # Call the ninjaTraining function to find the maximum points.
-print(ninjaTrainRec(points, 2, 3))
+print(ninjaTrainMemoized(points, 2, 3, dp))
