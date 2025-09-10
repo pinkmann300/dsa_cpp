2273f868ecf55391109cd3238ff7fdf9d925b163
Sandeep Chezhian A <sandeepchezhian@gmail.com>
2025-09-11 01:14:22 +0530

    Completed sum of subsets

--- data_structures/dp/lec3/subSumEq.py
+++ data_structures/dp/lec3/subSumEq.py
@@ -3,7 +3,6 @@ def subsetSumEqTok(ind, target, arr):
         return True 
     if (ind < 0): 
         return False 
-    
     else: 
         pickedCase = False 
         if (arr[ind] <= target): 
@@ -12,6 +11,39 @@ def subsetSumEqTok(ind, target, arr):
 
         return (pickedCase or unPickedCase) 
     
-arr = [2,3,4,5]
-k = subsetSumEqTok(len(arr) - 1, 7, arr) 
-print(k)  
+def subsetSumEqtoKMem(ind, target, arr, dp): 
+    if (target == 0): 
+        return True 
+    if (ind < 0): 
+        return False
+    if (dp[ind][target] != -1): 
+        return 
+    else: 
+        pickedCase = False 
+        if (arr[ind] <= target): 
+            pickedCase = subsetSumEqtoKMem(ind - 1, target - arr[ind], arr, dp) 
+        unPickedCase = subsetSumEqtoKMem(ind - 1, target, arr, dp)
+        dp[ind][target] = pickedCase or unPickedCase 
+        return dp[ind][target]
+
+def subMemo(target, arr):
+    dp = [[-1 for _ in range(target + 1)] for _ in range(len(arr))] 
+    k = subsetSumEqtoKMem(len(arr)-1, target, arr, dp)
+    return k 
+
+def subSumTab(target, arr): 
+    dp = [[False for _ in range(target + 1)] for _ in range(len(arr))]
+    for k in range(len(arr)): 
+        dp[k][0] = True 
+    for row in range(len(arr)): 
+        for col in range(1, target): 
+            pass 
+
+
+
+
+
+
+arr = [3,6,5]
+k = subMemo(8, arr) 
+print(k)
