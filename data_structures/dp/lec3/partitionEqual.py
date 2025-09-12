7b4fb1b385b6fb03587b9d5fabfd3991122956a8
Sandeep Chezhian A <sandeepchezhian@gmail.com>
2025-09-13 02:15:04 +0530

    Completed memoization approach

--- data_structures/dp/lec3/partitionEqual.py
+++ data_structures/dp/lec3/partitionEqual.py
@@ -5,7 +5,7 @@ def partitionSubsetHelper(arr):
     else:
         partitionSum = totalSum // 2 
         print(partitionSum)
-        k = partitionFinder(partitionSum, len(arr) - 1, arr) 
+        k = partitionMemoStart(partitionSum, len(arr) - 1, arr) 
         return k 
 
 
@@ -24,6 +24,31 @@ def partitionFinder(sum1, ind, arr):
     notTaken = partitionFinder(sum1, ind - 1, arr) 
 
     return taken or notTaken
+
+
+def partitionMemoizer(sum1, ind, arr, dp): 
+    if (sum1 == 0): 
+        return True 
+    if (ind == 0 and arr[ind] == sum1): 
+        return True 
+    if (ind < 0): 
+        return False 
+    if (dp[ind][sum1] != -1): 
+        return dp[ind][sum1] 
+    
+    taken = False 
+    if (arr[ind] <= sum1): 
+        taken = partitionMemoizer(sum1 - arr[ind], ind -1 , arr, dp) 
+    notTaken = partitionMemoizer(sum1, ind - 1, arr, dp)  
+    dp[ind][sum1] = taken or notTaken
+
+    return dp[ind][sum1] 
+ 
+ 
+def partitionMemoStart(sum1, ind, arr): 
+    dp = [[-1 for _ in range(sum1 + 1)] for _ in range(ind + 1)] 
+    finalResult = partitionMemoizer(sum1, ind, arr, dp) 
+    return finalResult
 
 
 arr = [2,3,3,3,4,5] 
