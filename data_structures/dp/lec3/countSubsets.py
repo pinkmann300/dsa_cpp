450321e5e3203f0748c76ce0879a524133c6329b
Sandeep Chezhian A <sandeepchezhian@gmail.com>
2025-09-14 13:48:55 +0530

    Completed count subsets using tabulation

--- data_structures/dp/lec3/countSubsets.py
+++ data_structures/dp/lec3/countSubsets.py
@@ -13,5 +13,53 @@ def countSubsets(arr, index, target):
 
     return taken + notTaken 
 
+
+def countMemoizer(arr, index, target, dp): 
+    if (target == 0): 
+        dp[index][target] = 1 
+        return dp[index][target]
+    if (index == 0 and arr[0] == target): 
+        dp[index][target] = 1
+        return dp[index][target]
+    if (index < 0): 
+        return 0
+    
+    if dp[index][target] != -1: 
+        return dp[index][target] 
+    
+    taken = 0 
+    if (arr[index] <= target): 
+        taken = countSubsets(arr, index - 1, target - arr[index])
+    notTaken = countSubsets(arr, index - 1, target) 
+
+    dp[index][target] = taken + notTaken
+    return dp[index][target] 
+
+
+def countMemStarter(arr, target):
+    dp = [[-1 for _ in range(target + 1)] for _ in range(len(arr))] 
+    countMemoizer(arr, len(arr) - 1, target, dp)
+    print(dp)
+    return dp[len(arr) - 1][target] 
+
+
+def countSubsets(arr, target): 
+    dpArr = [[0 for _ in range(target + 1)] for _ in range(len(arr))] 
+    for index in range(len(arr)): 
+        dpArr[index][0] = 1 
+
+    if arr[0] <= target: 
+        dpArr[0][arr[0]] = 1
+
+    for i in range(1, len(arr)): 
+        for j in range(1, target + 1): 
+            notTaken = dpArr[i - 1][j] 
+            taken = 0 
+            if (arr[i] <= j): 
+                taken = dpArr[i - 1][j - arr[i]] 
+
+            dpArr[i][j] = taken + notTaken
+    return dpArr[len(arr) - 1][target]
+
 arr = [1,2,2,3] 
-print(countSubsets(arr, len(arr) - 1, 4)) 
+print(countSubsets(arr, 3)) 
