5bf0442c6500c4e3994ff31c3e75af90110082db
Sandeep Chezhian A <sandeepchezhian@gmail.com>
2025-09-15 22:57:58 +0530

    Completed minimum coin denomination

--- data_structures/dp/lec3/coinDenom.py
+++ data_structures/dp/lec3/coinDenom.py
@@ -1,5 +1,5 @@
 def coinDenom(arr, target): 
-    return coinDenomMemStart(arr, target) 
+    return coinDenomTab(arr, target) 
 
 def coinDenomRec(arr, target, index): 
     if (target == 0): 
@@ -26,20 +26,35 @@ def coinDenomMem(arr, target, index, dp):
             return int(1e9) 
     if dp[index][target] != -1: 
         return dp[index][target] 
-    
     taken = int(1e9) 
     if (arr[index] <= target): 
         taken = 1 + coinDenomRec(arr, target - arr[index], index) 
-    notTaken = coinDenomRec(arr, target, index - 1) 
+    notTaken = 0 + coinDenomRec(arr, target, index - 1) 
     dp[index][target] = min(taken, notTaken) 
     return dp[index][target] 
-
 
 def coinDenomMemStart(arr, target): 
     dp = [[-1 for _ in range(target + 1)] for _ in range(len(arr))] 
     return coinDenomMem(arr, target, len(arr) - 1, dp) 
 
+def coinDenomTab(arr, target): 
+    dp = [[0 for _ in range(target + 1)] for _ in range(len(arr))] 
+    for i in range(target + 1): 
+        if i % arr[0] == 0: 
+            dp[0][i] = i // arr[0] 
+        else:
+            dp[0][i] = int(1e9) 
 
+    for row in range(1,len(arr)): 
+        for targetVal in range(target + 1):
+
+            notTaken = dp[row - 1][targetVal] 
+            taken = int(1e9) 
+            if (arr[row] <= targetVal): 
+                taken = 1 + dp[row][targetVal - arr[row]] 
+            dp[row][targetVal] = min(taken, notTaken) 
+
+    return dp[len(arr) - 1][target] 
 
     
 arr = [1,2,3] 
