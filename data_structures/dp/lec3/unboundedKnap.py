5708322039c6414642e3c7ab5425a3b44963d2ca
Sandeep Chezhian A <sandeepchezhian@gmail.com>
2025-09-16 01:48:40 +0530

    Completed unbounded knapsack using dynamic programming

--- data_structures/dp/lec3/unboundedKnap.py
+++ data_structures/dp/lec3/unboundedKnap.py
@@ -1,7 +1,7 @@
 def unboundedKnapSackRec(weight, wts, val, index): 
     if index == 0: 
-        if (wts >= weight[index]): 
-            return ((wts // weight[index]) * val[index])
+        if (wts >= weight[0]): 
+            return ((wts // weight[0]) * val[0])
         else: 
             return 0  
     taken = 0 
@@ -10,10 +10,31 @@ def unboundedKnapSackRec(weight, wts, val, index):
     notTaken = 0 + unboundedKnapSackRec(weight, wts, val, index - 1)
     return max(taken, notTaken) 
 
+def unboundedMemo(weight, wts, val, index, dp): 
+    if index == 0: 
+        if (wts >= weight[0]): 
+            return ((wts // weight[0]) * val[0])
+        else: 
+            return 0  
+    if dp[index][wts] != -1: 
+        return dp[index][wts] 
+    taken = 0 
+    if (weight[index] <= wts): 
+        taken = val[index] + unboundedKnapSackRec(weight, wts - weight[index], val, index) 
+    notTaken = 0 + unboundedKnapSackRec(weight, wts, val, index - 1)
+    dp[index][wts] = max(taken, notTaken) 
+    return dp[index][wts]
+
+def unboundedMemS(weight, wts, val): 
+    dp = [[-1 for _ in range(wts + 1)] for _ in range(len(weight))] 
+    return unboundedMemo(weight, wts, val,len(weight) - 1 ,dp) 
+
+
+
 wt = [2, 4, 6]
 val = [5, 11, 13]
 W = 10
-print(unboundedKnapSackRec(wt, W, val, 2))
+print(unboundedMemS(wt, W, val))
 
 
 
