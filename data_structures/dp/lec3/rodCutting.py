79900410a9f3a027401191cce3068e962d1c0884
Sandeep Chezhian A <sandeepchezhian@gmail.com>
2025-09-16 18:17:42 +0530

    Completed recursive solution

--- data_structures/dp/lec3/rodCutting.py
+++ data_structures/dp/lec3/rodCutting.py
@@ -1,5 +1,20 @@
 def rodCutting(arr): 
-    pass 
+    return rodCuttingRec(arr, len(arr), len(arr) - 1)
 
-def rodCuttingRec(arr, target, index): 
-    pass 
+
+def rodCuttingRec(arr, remLength, index): 
+    if (remLength == 0): 
+        return 0 
+    if (index == 0): 
+        return remLength * arr[0] 
+    notTaken = 0 + rodCuttingRec(arr, remLength, index - 1)
+    taken = 0 
+    if (remLength >= (index + 1)):
+        taken = arr[index] + rodCuttingRec(arr, remLength - index - 1, index) 
+
+    return max(taken, notTaken)
+     
+
+arr = [2,5,7,8,10] 
+k = rodCutting(arr) 
+print(k)
