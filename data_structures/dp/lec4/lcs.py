12a9830bb6777d760f816bf9a8f575fa85263f8f
Sandeep Chezhian A <sandeepchezhian@gmail.com>
2025-09-17 18:35:54 +0530

    Completed recursive implementation of printing longest common subsequence

--- data_structures/dp/lec4/lcs.py
+++ data_structures/dp/lec4/lcs.py
@@ -9,7 +9,22 @@ def longestCommonSub(s1, s2, ind1, ind2):
         s1Inclu = 0 + longestCommonSub(s1,s2, ind1 - 1, ind2) 
         s2Inclu = 0 + longestCommonSub(s1,s2, ind1, ind2 - 1) 
         return 0 + max(s1Inclu, s2Inclu)
-
     
 
-print(longestCommonSub("adebc", "dcadb", 4, 4))
+def longestCSubRec(s1, s2, ind1, ind2, cs): 
+    if (ind1 < 0 or ind2 < 0): 
+        return cs
+    if (s1[ind1] == s2[ind2]): 
+        return longestCSubRec(s1, s2, ind1 - 1, ind2 - 1, s1[ind1] + cs) 
+    else: 
+        s1Inclu = longestCSubRec(s1, s2, ind1 - 1, ind2, cs) 
+        s2Inclu = longestCSubRec(s1, s2, ind1, ind2 - 1, cs) 
+        maxVal = "" 
+        if (len(s1Inclu) > len(s2Inclu)): 
+            maxVal = s1Inclu 
+        else: 
+            maxVal = s2Inclu 
+
+        return maxVal     
+
+print(longestCSubRec("adebc", "dcadb", 4, 4, ""))
