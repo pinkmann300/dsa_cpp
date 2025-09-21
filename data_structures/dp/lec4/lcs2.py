ab2261b449b423b64910ba98dea79ba1943e3f25
Sandeep Chezhian A <sandeepchezhian@gmail.com>
2025-09-21 14:51:23 +0530

    Completed lcs2

--- data_structures/dp/lec4/lcs2.py
+++ data_structures/dp/lec4/lcs2.py
@@ -1,16 +1,10 @@
 def longestCSRec(s1, s2, ind1, ind2, count):
-    # Base case: if we go out of bounds
     if ind1 < 0 or ind2 < 0:
         return count
-
-    # If characters match, extend the substring
     if s1[ind1] == s2[ind2]:
         count = longestCSRec(s1, s2, ind1 - 1, ind2 - 1, count + 1)
-
-    # Otherwise, reset count but explore both options
     k = longestCSRec(s1, s2, ind1 - 1, ind2, 0)
     r = longestCSRec(s1, s2, ind1, ind2 - 1, 0)
-
     return max(count, k, r)
 
 def longestCommonSubstringTabulation(s1, s2): 
@@ -24,10 +18,15 @@ def longestCommonSubstringTabulation(s1, s2):
                 dp[i][j] = 1 + dp[i - 1][j - 1] 
                 ans = max(dp[i][j], ans) 
             else: 
-                dp[i][j] = 0 
+                dp[i][j] = 
     return ans
 
+def longestPalindromicSub(s1): 
+    reverseStr = s1[::-1] 
+    return longestCommonSubstringTabulation(s1, reverseStr)
+
 print(longestCSRec("asandeepwe", "csandeepa", len("asandeepwe") - 1, len("csandeepa") - 1, 0))
+print(longestPalindromicSub("bbabcbcab"))
 
 
 
