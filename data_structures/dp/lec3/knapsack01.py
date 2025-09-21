50fbdc15a4c9dd0f170bfc9ac610d5264bd842af
Sandeep Chezhian A <sandeepchezhian@gmail.com>
2025-09-21 23:25:28 +0530

    Completed knapsack problem

--- data_structures/dp/lec3/knapsack01.py
+++ data_structures/dp/lec3/knapsack01.py
@@ -3,7 +3,7 @@ def knapsack(index, weight, weights, vals):
         return 0 
     if (index == 0): 
         if (weights[index] <= weight): 
-            return vals[index] 
+            return vals[index]
         else: 
             return 0 
     notTaken = 0 + knapsack(index - 1, weight, weights, vals) 
@@ -14,9 +14,25 @@ def knapsack(index, weight, weights, vals):
     return max(taken, notTaken) 
 
 
+def knapsackTab(weight, weights, val): 
+    dp = [[0 for _ in range(weight + 1)] for _ in range(len(weights))]
+    for i in range(weight + 1): 
+        if (i >= weights[0]):
+            dp[0][i] = val[0] 
+    ans = 0 
+    for index2 in range(1, len(weights)): 
+        for weightIndex in range(weight + 1):
+            taken = -1 
+            notTaken = dp[index2][weightIndex] 
+            if (weights[index2] <= weightIndex): 
+                taken = val[index2] +  dp[index2 - 1][weightIndex - weights[index2]]
+            dp[index2][weightIndex] = max(taken, notTaken) 
+            ans = max(dp[index2][weightIndex], ans) 
+    return ans 
+
 
 wt = [5, 4, 2, 3]
 val = [10, 40, 30, 50] 
 weight = 5
 
-print(knapsack(len(wt) - 1, weight, wt, val)) 
+print(knapsackTab(weight, wt, val)) 
