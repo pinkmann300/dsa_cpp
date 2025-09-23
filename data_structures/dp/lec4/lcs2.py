720e4c056f07af690be081afe788fbe460e764d5
Sandeep Chezhian A <sandeepchezhian@gmail.com>
2025-09-23 07:39:36 +0530

    Completed work

--- data_structures/dp/lec4/lcs2.py
+++ data_structures/dp/lec4/lcs2.py
@@ -3,6 +3,7 @@ def longestCSRec(s1, s2, ind1, ind2, count):
         return count
     if s1[ind1] == s2[ind2]:
         count = longestCSRec(s1, s2, ind1 - 1, ind2 - 1, count + 1)
+
     k = longestCSRec(s1, s2, ind1 - 1, ind2, 0)
     r = longestCSRec(s1, s2, ind1, ind2 - 1, 0)
     return max(count, k, r)
@@ -19,14 +20,12 @@ def longestCommonSubstringTabulation(s1, s2):
                 ans = max(dp[i][j], ans) 
             else: 
                 dp[i][j] = 0 
-
     return ans
 
 def longestPalindromicSub(s1): 
     reverseStr = s1[::-1] 
     return longestCommonSubstringTabulation(s1, reverseStr)
 
-#print(longestCSRec("asandeepwe", "csandeepa", len("asandeepwe") - 1, len("csandeepa") - 1, 0))
 print(longestCommonSubstringTabulation("bbabcbcab", "bbabcbcb"))
 
 
