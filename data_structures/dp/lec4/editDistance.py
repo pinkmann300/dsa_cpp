dcbb7460567e6f7ed494f2b29a958e9161757b49
Sandeep Chezhian A <sandeepchezhian@gmail.com>
2025-09-26 22:16:41 +0530

    Completed tabulation method

--- data_structures/dp/lec4/editDistance.py
+++ data_structures/dp/lec4/editDistance.py
@@ -27,9 +27,32 @@ def editDist(ind1, ind2, s1, s2, dp):
 
 def editMemo(s1, s2): 
     dp = [[-1 for _ in range(len(s2))] for _ in range(len(s1))] 
-    return editDist(len(s1) - 1, len(s2) - 1, s1, s2, dp) 
+    return editDist(len(s1) - 1, len(s2) - 1, s1, s2, dp)
+
+def editDistTabulation(s1, s2): 
+    ind1 = len(s1) 
+    ind2 = len(s2) 
+
+    dp = [[0 for _ in range(ind2 + 1)] for _ in range(ind1 + 1)] 
+    for i in range(ind1 + 1): 
+        dp[i][0] = i 
+
+    for j in range(ind2 + 1): 
+        dp[0][j] = j   
+
+    for i in range(1, ind1 + 1): 
+        for j in range(1, ind2 + 1): 
+            if (s1[i - 1] == s2[j - 1]): 
+                dp[i][j] = dp[i - 1][j - 1] 
+            else: 
+                dp[i][j] = 1 + min(dp[i - 1][j - 1], dp[i][j - 1], dp[i - 1][j]) 
+    
+    return dp[ind1][ind2] 
+
+
+
 
 s1 = "horse"
 s2 = "ros"
     # Calculate and print the minimum number of operations required
-print("The minimum number of operations required is:", editMemo(s1, s2))
+print("The minimum number of operations required is:", editDistTabulation(s1, s2))
