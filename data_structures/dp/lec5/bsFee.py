a54e03a5e89d92acab920477a399e52d6d9299dc
Sandeep Chezhian A <sandeepchezhian@gmail.com>
2025-09-27 12:08:49 +0530

    Completed memoization

--- data_structures/dp/lec5/bsFee.py
+++ data_structures/dp/lec5/bsFee.py
@@ -12,10 +12,34 @@ def bsFee(arr, ind, buy, fee):
 
     return max(op1, op2) 
 
+def bsFee2(arr, ind, buy, fee, dp): 
+    if (ind == len(arr)): 
+        return 0 
+    
+    if dp[ind][buy] != -1: 
+        return dp[ind][buy] 
+    
+    if buy == 0: 
+        op1 = -arr[ind] + bsFee2(arr, ind + 1, 1, fee, dp) 
+        op2 = 0 + bsFee2(arr, ind + 1, 0, fee, dp) 
+
+    if buy == 1: 
+        op1 = arr[ind] - fee + bsFee2(arr, ind + 1, 0, fee, dp)
+        op2 = 0 + bsFee2(arr, ind + 1, 1, fee, dp) 
+
+    dp[ind][buy] = max(op1, op2) 
+
+    return dp[ind][buy] 
+
+def bsMemo(arr, fee): 
+    dp = [[-1 for _ in range(2)] for _ in range(len(arr))]
+    return (bsFee2(arr, 0, 0, fee, dp)) 
+
+
+
 
 prices = [1, 3, 2, 8, 4, 9]
 n = len(prices)
 fee = 2
-
-result = bsFee(prices, 0, 0, fee) 
+result = bsMemo(prices,fee) 
 print(f"The maximum profit that can be generated is {result}")
