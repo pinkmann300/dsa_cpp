4f08d1587a7faef78b5d8bea2305b6fb01884f3b
Sandeep Chezhian A <sandeepchezhian@gmail.com>
2025-09-28 19:12:58 +0530

    Completed buy sell with cap

--- data_structures/dp/lec5/bsCap.py
+++ data_structures/dp/lec5/bsCap.py
@@ -35,8 +35,27 @@ def bsCapRecMemo(arr, ind, cap, buy, dp):
 def bsCapMemStart(arr): 
     dp = [[[-1 for _ in range(3)] for _ in range(2)] for _ in range(len(arr))] 
     return bsCapRecMemo(arr, 0, 2, 0, dp) 
+
+
+def bsCapTabulate(arr): 
+    dp = [[[0 for _ in range(3)] for _ in range(2)] for _ in range(len(arr) + 1)] 
+    for i in range(len(arr) - 1, -1, -1): 
+        for j in range(2): 
+            for k in range(1,3): 
+                if j == 0: 
+                    op1 = -arr[i] + dp[i + 1][1][k] 
+                    op2 = dp[i + 1][0][k] 
+
+                if j == 1: 
+                    op1 = arr[i] + dp[i + 1][0][k - 1] 
+                    op2 = dp[i + 1][1][k] 
+
+                dp[i][j][k] = max(op1, op2) 
+    return dp[0][0][2] 
+
+
  
 
 prices = [3, 3, 5, 0, 0, 3, 1, 4]
-max_profit = bsCapMemStart(prices)  
+max_profit = bsCapTabulate(prices)  
 print(max_profit)
