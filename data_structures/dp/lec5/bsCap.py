e2f94b54e0a4f9038066430982027c73ef876cdb
Sandeep Chezhian A <sandeepchezhian@gmail.com>
2025-09-28 19:00:44 +0530

    Completed

--- data_structures/dp/lec5/bsCap.py
+++ data_structures/dp/lec5/bsCap.py
@@ -12,6 +12,31 @@ def bsCapRec(arr, ind, cap, buy):
 
     return max(op1, op2) 
 
+
+def bsCapRecMemo(arr, ind, cap, buy, dp): 
+    if (ind == len(arr) or cap == 0): 
+        return 0 
+    
+    if dp[ind][buy][cap] != -1: 
+        return dp[ind][buy][cap] 
+    
+    if buy == 0: 
+        op1 = -arr[ind] + bsCapRecMemo(arr, ind + 1, cap, 1, dp) 
+        op2 = 0 + bsCapRecMemo(arr, ind + 1, cap, 0, dp) 
+
+    if buy == 1: 
+        op1 = arr[ind] + bsCapRecMemo(arr, ind + 1, cap - 1, 0, dp) 
+        op2 = 0 + bsCapRecMemo(arr, ind + 1, cap, 1, dp) 
+
+    dp[ind][buy][cap] = max(op1, op2)   
+    return dp[ind][buy][cap] 
+
+
+def bsCapMemStart(arr): 
+    dp = [[[-1 for _ in range(3)] for _ in range(2)] for _ in range(len(arr))] 
+    return bsCapRecMemo(arr, 0, 2, 0, dp) 
+ 
+
 prices = [3, 3, 5, 0, 0, 3, 1, 4]
-max_profit = bsCapRec(prices, 0, 2, 0) 
+max_profit = bsCapMemStart(prices)  
 print(max_profit)
