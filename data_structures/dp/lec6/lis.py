47469c43d19e4c9672b504511160beaa0e6314e5
Sandeep Chezhian A <sandeepchezhian@gmail.com>
2025-09-28 22:12:08 +0530

    Completed

---
+++ data_structures/dp/lec6/lis.py
@@ -0,0 +1,18 @@
+def lis(ind, arr, prev_index): 
+    if (ind == len(arr)): 
+        return 0
+    
+    notTaken = 0 + lis(ind + 1, arr, prev_index) 
+    taken = 0 
+
+    if (arr[ind] > arr[prev_index] or prev_index == -1 ): 
+        taken = 1 + lis(ind + 1, arr, ind) 
+
+    return max(taken, notTaken) 
+
+
+prices = [4,6,2,8] 
+print(lis(0,prices, -1)) 
+
+
+
