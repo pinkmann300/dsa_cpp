f32d6534e8206bd4f4a8755bcf7738daf9f9367c
Sandeep Chezhian A <sandeepchezhian@gmail.com>
2025-09-29 00:15:37 +0530

    Completed longest increasing subsequence function

--- data_structures/dp/lec6/lis.py
+++ data_structures/dp/lec6/lis.py
@@ -1,18 +1,34 @@
 def lis(ind, arr, prev_index): 
     if (ind == len(arr)): 
         return 0
-    
     notTaken = 0 + lis(ind + 1, arr, prev_index) 
     taken = 0 
-
     if (arr[ind] > arr[prev_index] or prev_index == -1 ): 
         taken = 1 + lis(ind + 1, arr, ind) 
-
     return max(taken, notTaken) 
 
 
+def lisMem(ind, prev_index, arr, dp): 
+    if ind == len(arr): 
+        return 0 
+    
+    if dp[ind][prev_index + 1] != -1: 
+        return dp[ind][prev_index + 1] 
+    
+    taken = 0 
+    notTaken = 0 + lisMem(ind + 1, prev_index, arr, dp) 
+    if (arr[ind] > arr[prev_index] or prev_index == -1): 
+        taken = 1 + lisMem(ind + 1, ind, arr, dp) 
+
+    dp[ind][prev_index + 1] = max(taken, notTaken) 
+    return dp[ind][prev_index + 1]
+
+
+def lisMemStart(arr): 
+    dp = [[-1 for _ in range(len(arr) + 1)] for _ in range(len(arr))] 
+    return lisMem(0, -1, arr, dp) 
+
+
+
 prices = [7,6,2,8] 
-print(lis(0,prices, -1)) 
-
-
-
+print(lisMemStart(prices)) 
