3e926557afcb941353df9a718fe34c74eb89c9b0
Sandeep Chezhian A <sandeepchezhian@gmail.com>
2025-09-28 18:54:21 +0530

    Completed capped transactions

--- data_structures/dp/lec5/bsCap.py
+++ data_structures/dp/lec5/bsCap.py
@@ -1,1 +1,17 @@
-def bsCapRec(arr, ind, cap, )
+def bsCapRec(arr, ind, cap, buy): 
+    if (ind == len(arr) or cap == 0): 
+        return 0 
+    
+    if buy == 0:
+        op1 = -arr[ind] + bsCapRec(arr, ind + 1, cap, 1) 
+        op2 = 0 + bsCapRec(arr, ind + 1, cap, 0)
+
+    if buy == 1: 
+        op1 = arr[ind] + bsCapRec(arr, ind + 1, cap - 1, 0) 
+        op2 = bsCapRec(arr, ind + 1, cap, 1) 
+
+    return max(op1, op2) 
+
+prices = [3, 3, 5, 0, 0, 3, 1, 4]
+max_profit = bsCapRec(prices, 0, 2, 0) 
+print(max_profit)
