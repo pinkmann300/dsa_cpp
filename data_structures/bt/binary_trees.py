ebedd22b5bfad39a5dc1e58a438f799d8b201874
Sandeep Chezhian A <sandeepchezhian@gmail.com>
2025-10-03 03:07:07 +0530

    Completed traversals through recursion

--- data_structures/bt/binary_trees.py
+++ data_structures/bt/binary_trees.py
@@ -23,4 +23,34 @@ def heightOfTree(root):
         return max(left, right) 
 
 
-print(heightOfTree(binTree1))
+def preOrder(tree, arr): 
+    if tree is None: 
+        return
+    else: 
+        arr.append(tree.data) 
+        preOrder(tree.left, arr) 
+        preOrder(tree.right, arr) 
+
+def postOrder(tree,arr): 
+    if tree is None: 
+        return 
+    else: 
+        postOrder(tree.left, arr) 
+        postOrder(tree.right, arr) 
+        arr.append(tree.data) 
+
+def inOrder(tree, arr): 
+    if tree is None: 
+        return  
+    else: 
+        inOrder(tree.left, arr) 
+        arr.append(tree.data) 
+        inOrder(tree.right, arr) 
+
+
+def traversalStart(tree): 
+    arr = [] 
+    postOrder(tree, arr)
+    return arr  
+
+print(traversalStart(binTree1))
