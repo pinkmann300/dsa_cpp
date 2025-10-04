15bc759a1eea8ab0fe062a259debf21a79b264d3
Sandeep Chezhian A <sandeepchezhian@gmail.com>
2025-10-04 19:28:16 +0530

    Completed assign cookies greedy algorithm pt1

---
+++ data_structures/greedy_algo/assignCookies.py
@@ -0,0 +1,18 @@
+def assignCookies(students, cookie): 
+    studentIndex = 0 
+    cookieIndex = 0 
+
+    students.sort() 
+    cookie.sort() 
+
+    while cookieIndex < len(cookie) and studentIndex < len(students): 
+        if (cookie[cookieIndex] >= students[studentIndex]): 
+            studentIndex += 1 
+        cookieIndex += 1 
+
+    return studentIndex
+
+student = [1, 2, 3]
+cookie = [1, 1]
+
+print(assignCookies(student, cookie))
