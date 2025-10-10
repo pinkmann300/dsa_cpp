6eb392cca9b73cb3f7f420e93fb26ff5bd320bd8
Sandeep Chezhian A <sandeepchezhian@gmail.com>
2025-10-11 04:12:45 +0530

    Completed inorder traversal

--- data_structures/bt/traversals/inOrder.py
+++ data_structures/bt/traversals/inOrder.py
@@ -17,5 +17,26 @@ def inOrderStart(tree):
     inOrder(tree, arr) 
     return arr 
 
+def inOrderIterative(tree): 
+    inOrderArray = [] 
+    traversalStack = []
+    if tree is None: 
+        return inOrderArray 
+    
+    current = tree 
+    while current or traversalStack:
+        while current:  
+            traversalStack.append(current) 
+            current = current.left
+        
+        current = traversalStack.pop() 
+        inOrderArray.append(current.data) 
+        current = current.right 
+    
+    return inOrderArray
+
+    
+    
+
 binaryTree = BinaryTree(3, BinaryTree(4, None, BinaryTree(5, None))) 
 print(inOrderStart(binaryTree))
