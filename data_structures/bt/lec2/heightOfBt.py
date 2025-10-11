56d3bc22a9041297789fc9df58000be7751b826f
Sandeep Chezhian A <sandeepchezhian@gmail.com>
2025-10-11 11:37:32 +0530

    Completed computation of height of binary tree

---
+++ data_structures/bt/lec2/heightOfBt.py
@@ -0,0 +1,45 @@
+from collections import deque
+
+class BTree: 
+    def __init__(self, data = None, left = None, right = None): 
+        self.data = data 
+        self.left = left 
+        self.right = right 
+
+def heightOfBt(tree): 
+    if not tree: 
+        return 0 
+    left = 1 + heightOfBt(tree.left) 
+    right = 1 + heightOfBt(tree.right) 
+    return max(left, right) 
+
+binTree1 = BTree(5, BTree(6, None, BTree(7, None)), BTree(6, BTree(4, BTree(5, None, None), None), None)) 
+print(heightOfBt(binTree1))
+
+def levelOrder(tree): 
+    if not tree:
+        return 0 
+    
+    q = deque()
+    levelOrderArray = [] 
+    q.append(tree) 
+    while q: 
+        size = len(q) 
+        levels = [] 
+        for _ in range(size): 
+            node = q.popleft() 
+
+            levels.append(node.data) 
+            levelOrderArray = levelOrderArray + [levels] 
+
+            if (node.left):
+                q.append(node.left) 
+
+            if (node.right):
+                q.append(node.right) 
+
+
+    return levelOrderArray 
+            
+        
+print(levelOrder(binTree1))
