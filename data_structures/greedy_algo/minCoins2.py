ef0c85d731de8288129d8e4f4d87f7f3ce17a8ea
Sandeep Chezhian A <sandeepchezhian@gmail.com>
2025-11-27 23:50:43 +0530

    Jump game completed

--- data_structures/greedy_algo/minCoins2.py
+++ data_structures/greedy_algo/minCoins2.py
@@ -19,3 +19,20 @@ def minCoins(arr, target):
 arr = [ 1, 2, 5, 10, 20, 50, 100, 500, 1000 ] 
 print(minCoins(arr, 121))
 
+def jumpGame(arr): 
+    maxIndexReached = 0 
+    current_index = 0 
+
+    while maxIndexReached < len(arr) - 1 and current_index <= len(arr) - 1: 
+        if current_index > maxIndexReached: 
+            return False 
+    
+        reachableIndex = current_index + arr[current_index] 
+        maxIndexReached = max(maxIndexReached, reachableIndex) 
+        current_index += 1 
+
+    return True 
+
+
+
+    
