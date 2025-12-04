68b842b8314dccbb1a5c27dfbd0c2026dfb07179
Sandeep Chezhian A <sandeepchezhian@gmail.com>
2025-12-04 22:14:17 +0530

    Completed graphs study

---
+++ data_structures/graphs/graphTheory.py
@@ -0,0 +1,20 @@
+"""
+The below is the basic notes for graphs in my Data structures and algorithms 
+course. Graphs has 54 problems on takeUfoward and we will solve all of them under 
+this directory. 
+
+
+Lecture 1 : Types, Conventions and preliminaries. 
+
+Difference between a directed graph and an undirected graph is that the directionality 
+of an edge is specified in a directed graph and not in an undirected graph.
+
+A graph is said to be cyclic if there exists a node such that there exists a path in the graph 
+which starts from a particular vertex and ends at the same vertex. 
+
+
+"""
+
+
+
+
