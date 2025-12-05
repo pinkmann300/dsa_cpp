0ea850cfa4cb37ba557578e278f5f6f347be107c
Sandeep Chezhian A <sandeepchezhian@gmail.com>
2025-12-05 13:14:32 +0530

    Completed adjacency matrix representation for graphs

--- data_structures/graphs/graphTheory.py
+++ data_structures/graphs/graphTheory.py
@@ -3,7 +3,6 @@ The below is the basic notes for graphs in my Data structures and algorithms
 course. Graphs has 54 problems on takeUfoward and we will solve all of them under 
 this directory. 
 
-
 Lecture 1 : Types, Conventions and preliminaries. 
 
 Difference between a directed graph and an undirected graph is that the directionality 
@@ -16,7 +15,6 @@ A graph is said to a directed cyclic graph if there exists a node such that ther
 path from that node to itself. 
 
 A directed graph which does not have a cycle in it is called a directed acyclic graph (DAG). 
-
 
 Definition of a path:
 Contains a lot of nodes and each of them are reachable. A particular node cannot appear twice in a path. 
@@ -32,9 +30,30 @@ is the number of outgoing edges from a node.
 Some graphs which are called weighted graphs have edges with a number attached to them. The number on the edge denotes the 
 weight on the particular edge.  
 
-
 """
 
+def createAdjacenyMatrix(vertices, edges, edgeList): 
+    adjMatrix = [[0 for _ in range(vertices + 1)] for _ in range(vertices + 1)]
+
+    for i in range(0, edges): 
+        node1, node2 = edgeList[i] 
+        adjMatrix[node1][node2] = 1 
+        adjMatrix[node2][node1] = 1 
+
+    return adjMatrix
+ 
+"""
+Representing a graph in the form of an adjacency matrix is a little expensive as it takes a space complexity of 
+O(n^2). 
+"""
+
+def main(): 
+    numberOfVertices = 5 
+    numberOfEdges = 6 
+    edgeList = [(1,2), (1,3), (2,4), (3,4), (3,5), (4,5)]
+    adjacencyMatrix = createAdjacenyMatrix(numberOfVertices, numberOfEdges, edgeList) 
+    print("ADJACENCY MATRIX : \n", adjacencyMatrix) 
 
 
 
+main() 
