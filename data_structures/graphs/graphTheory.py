a27d814567a59a403ebfb59e89137fc9d85b9624
Sandeep Chezhian A <sandeepchezhian@gmail.com>
2025-12-05 12:29:36 +0530

    Added notes

--- data_structures/graphs/graphTheory.py
+++ data_structures/graphs/graphTheory.py
@@ -12,6 +12,26 @@ of an edge is specified in a directed graph and not in an undirected graph.
 A graph is said to be cyclic if there exists a node such that there exists a path in the graph 
 which starts from a particular vertex and ends at the same vertex. 
 
+A graph is said to a directed cyclic graph if there exists a node such that there is a directed 
+path from that node to itself. 
+
+A directed graph which does not have a cycle in it is called a directed acyclic graph (DAG). 
+
+
+Definition of a path:
+Contains a lot of nodes and each of them are reachable. A particular node cannot appear twice in a path. 
+
+
+Degrees in a graph: The number of edges from a particular vertex is called the degree of that vertex. The sum 
+of degrees of all vertices in a graph is called the degree of the graph. Total degree of a graph = twice the number of edges. 
+Every edge is associated to gtwo nodes and hence the above property. 
+
+The indegree of a node in a directed graph is the number of incoming edges into a node. The outdegree of a vertex 
+is the number of outgoing edges from a node. 
+
+Some graphs which are called weighted graphs have edges with a number attached to them. The number on the edge denotes the 
+weight on the particular edge.  
+
 
 """
 
