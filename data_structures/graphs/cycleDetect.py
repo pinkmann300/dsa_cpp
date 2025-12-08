d4ff0f90ba80a7c7c35ccefe2838d86aa8343321
Sandeep Chezhian A <sandeepchezhian@gmail.com>
2025-12-08 23:06:43 +0530

    Completed dfs for cycle detection

---
+++ data_structures/graphs/cycleDetect.py
@@ -0,0 +1,21 @@
+from collections import deque 
+
+def cycleDetection(adjList, srcNode, visited): 
+    if not adjList: 
+        return False 
+    
+    queue = deque() 
+    queue.append((srcNode, -1)) 
+    visited[1] = True 
+
+    while queue: 
+        node, parentNode = queue.popleft() 
+        adjacentNodes = adjList[node] 
+
+        for i in adjacentNodes: 
+            if not visited[i]: 
+                queue.append((i, node)) 
+            elif parentNode != i: 
+                return True 
+            
+    return False
