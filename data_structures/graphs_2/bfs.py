18cdda4ffde9d6283ecbbaa3b09259cc5f0836c9
Sandeep Chezhian A <sandeepchezhian@gmail.com>
2026-01-03 00:36:12 +0530

    Completed rotten oranges

--- data_structures/graphs_2/bfs.py
+++ data_structures/graphs_2/bfs.py
@@ -20,3 +20,57 @@ def bfs(adjacencyList, V):
                         que.append(nodes) 
 
     return bfs_list       
+
+
+def rottenOranges(matrix): 
+    totalOranges = 0 
+    rottenOranges = deque()
+
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0])):
+            if matrix[i][j] != 0: 
+                if matrix[i][j] == 2: 
+                    rottenOranges.append((i, j)) 
+                totalOranges += 1 
+
+    minutes = 0 
+    rottenCount = 0 
+
+    directionVectors = [(0, 1), (0, -1), (1, 0), (-1, 0)] 
+
+    while rottenOranges: 
+        rottenCount += len(rottenOranges) 
+        k = len(rottenOranges) 
+
+        for _ in range(k): 
+            rottenX, rottenY = rottenOranges.popleft()
+
+            for dx, dy in directionVectors: 
+                nx = rottenX + dx 
+                ny = rottenY + dy 
+
+                if nx < 0 or nx >= len(matrix) or ny < 0 or ny >= len(matrix[0]) or matrix[nx][ny] != 1: 
+                    continue 
+
+                matrix[nx][ny] = 2 
+                rottenOranges.append((nx, ny)) 
+
+        if rottenOranges: 
+            minutes += 1 
+
+
+    return minutes if rottenCount == totalOranges else -1 
+    
+
+
+
+
+
+
+
+
+
+    
+    
+
+            
