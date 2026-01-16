c3de7007da7e6d9a4c3b6fb497b865b0aa546365
Sandeep Chezhian A <sandeepchezhian@gmail.com>
2026-01-17 02:18:43 +0530

    Completed shortest distance in undirected unit weight graph

--- data_structures/graphs/shortestDistance.py
+++ data_structures/graphs/shortestDistance.py
@@ -1,5 +1,34 @@
-from collections import
+from collections import deque
 
-def shortestDistance(adjacencyList): 
+def shortestDistance(adjacencyList, source): 
     distanceArray = [float('inf') for _ in range(len(adjacencyList))] 
     queue = deque() 
+
+    queue.append((source, 0)) 
+    distanceArray[source] = 0  
+
+    while queue: 
+        node, distance = queue.popleft() 
+        for neighbour in adjacencyList[node]: 
+            newDistance = distance + 1 
+            if newDistance < distanceArray[neighbour]:
+                queue.append((neighbour, newDistance))
+            
+
+    resultantArray = [0 for _ in range(len(adjacencyList))] 
+
+    for k in range(len(distanceArray)):
+        if distanceArray[k] != float('inf'): 
+            resultantArray[k] = distanceArray[k] 
+        else: 
+            resultantArray[k] = -1 
+
+
+    return resultantArray
+
+         
+
+
+
+
+    
