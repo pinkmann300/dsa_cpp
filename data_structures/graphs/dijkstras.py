a6a3e4de754d008b742079b42174d900624a50fb
Sandeep Chezhian A <sandeepchezhian@gmail.com>
2026-01-19 04:50:09 +0530

    Dijkstra explnation added

---
+++ data_structures/graphs/dijkstras.py
@@ -0,0 +1,6 @@
+""" 
+The code below describes the Dijkstra's algorithm for finding the shortest distance 
+from a source node to any other node in the graph given. Dijkstra's algorithm is very 
+similar to the BFS algorithm. 
+
+"""
