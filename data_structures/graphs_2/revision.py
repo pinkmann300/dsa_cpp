300d8b28048d6d01486e09d785430a117641ef6b
Sandeep Chezhian A <sandeepchezhian@gmail.com>
2026-02-12 13:05:27 +0530

    Completed number of provinces problem

--- data_structures/graphs_2/revision.py
+++ data_structures/graphs_2/revision.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 """ 
 Assuming the graph using 0 based indexing
 """
@@ -12,6 +14,15 @@ def createAdjacenyList(numberOfVertices, edges):
     return adjacencyList 
 
 
+def createAdjacencyListFromMatrix(matrix): 
+    adjacencyList = [[] for _ in range(len(matrix))]
+    for i in range(len(matrix)): 
+        for j in range(len(matrix)): 
+            if matrix[i][j] == 1: 
+                adjacencyList[i].append(j) 
+    
+    return adjacencyList
+ 
 def dfsStart(adjacencyList): 
     visitedArray = [0 for _ in range(len(adjacencyList))] 
     resultantArray = [] 
@@ -30,3 +41,30 @@ def dfs(adjacencyList, srcNode, visitedArray, resultant):
     for adjNodes in adjacencyList[srcNode]: 
         if visitedArray[adjNodes] != 1: 
             dfs(adjacencyList, adjNodes, visitedArray, resultant)
+
+
+def numberOfProvinces(connectionMatrix): 
+    adjacencyList = createAdjacencyListFromMatrix(connectionMatrix) 
+    visitedArray = [0 for _ in range(len(adjacencyList))] 
+    count = 0 
+
+    for i in range(len(adjacencyList)): 
+        if visitedArray[i] != 1: 
+            queue = deque() 
+            queue.append(i) 
+
+            while queue:
+                node = queue.popleft() 
+                visitedArray[node] = 1 
+
+                for adjNodes in adjacencyList[node]:
+                    if visitedArray[adjNodes] != 1: 
+                        queue.append(adjNodes) 
+
+        count += 1 
+
+    return count 
+
+
+        
+
