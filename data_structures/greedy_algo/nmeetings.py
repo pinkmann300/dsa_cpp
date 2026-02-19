46baf6e6c34539158eba68f93885a8f079b7259e
Sandeep Chezhian A <sandeepchezhian@gmail.com>
2026-02-20 00:10:31 +0530

    compelteD

--- data_structures/greedy_algo/nmeetings.py
+++ data_structures/greedy_algo/nmeetings.py
@@ -18,3 +18,31 @@ def nMeetings(start, end):
 #Language based learning is that if I have a list of tuples and I try to sort it, 
 #it sorts based on the first element in the tuple. 
 
+
+"""
+Example 1:
+Input:nums = [2, 3, 1, 0, 4]
+Output: True           
+Explanation: 
+We start at index 0, with value 2 this means we can jump to index 1 or 2.
+From index 1, with value 3, we can jump to index 2, 3, or 4. However, if we jump to index 2 with value 1, we can only jump to index 3.
+So we jump to index 1 then index 4 reaching the end of the array.
+Hence, we return true.
+
+"""
+def jumpGame(arr): 
+    maximumIndexReached = 0 
+    currentIndex = 0 
+
+    while currentIndex <= maximumIndexReached: 
+        maximumIndexReached += arr[currentIndex]
+        if maximumIndexReached >= len(arr) - 1: 
+            return True 
+
+    return False 
+ 
+
+nums = [3,2,1,0,4] 
+print(jumpGame(nums))
+
+
