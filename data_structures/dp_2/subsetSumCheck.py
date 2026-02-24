e85f14b1899f772b0d53ac2495f2587ce4706948
Sandeep Chezhian A <sandeepchezhian@gmail.com>
2026-02-25 00:35:08 +0530

    Completed solution for subsetSum check

--- data_structures/dp_2/subsetSumCheck.py
+++ data_structures/dp_2/subsetSumCheck.py
@@ -17,6 +17,28 @@ def subsetSumCheck(index, target, arr):
 def mainSubsetSum(target, arr): 
     return subsetSumCheck(0, target, arr) 
 
-arr = [1,2,3,4] 
-print(mainSubsetSum(15, arr)) 
+def tabulatedSolution(target, arr): 
+    dp = [[False for _ in range(target + 1)] for _ in range(len(arr))]
 
+    for k in range(len(arr)): 
+        dp[k][0] = True 
+
+    if arr[0] <= target: 
+        dp[0][arr[0]] = True
+
+    for i in range(1, len(arr)): 
+        for j in range(1, target + 1):  
+            taken = False 
+            if arr[i] <= j:
+                taken = dp[i - 1][j - arr[i]] 
+
+            notTaken = dp[i - 1][j] 
+            dp[i][j] = taken or notTaken
+
+    return dp[len(arr) - 1][target] 
+
+newArr = [1,2,3,4] 
+target = 15 
+
+
+print(tabulatedSolution(target, newArr))
