d4ae6b294a5fb9c481323f409eaa5a041600fca9
Sandeep Chezhian A <sandeepchezhian@gmail.com>
2026-02-27 14:38:29 +0530

    Completed min coins using tabulation

---
+++ data_structures/dp_2/minCoins.py
@@ -0,0 +1,34 @@
+def recusiveMinCoins(arr, index, target): 
+    if index == 0: 
+        if target % arr[index] == 0: 
+            return (target // arr[index])
+        else: 
+            return float('inf') 
+        
+    take = float('inf') 
+    notTake = 0 + recusiveMinCoins(arr, index - 1, target) 
+    if arr[index] <= target: 
+        take = 1 + recusiveMinCoins(arr, index, target - arr[index]) 
+    
+    return min(take, notTake) 
+
+def minCoinsTabulated(arr, target): 
+    dp = [[float('inf') for _ in range(target + 1)] for _ in range(len(arr))] 
+
+    for i in range(target + 1):
+        if i % arr[0] == 0: 
+            dp[0][i] = i // arr[0]
+
+    for row in range(1, len(arr)): 
+        for col in range(target + 1): 
+            taken = float('inf')
+            notTaken = 0 + dp[row - 1][col] 
+            if arr[row] <= col: 
+                taken = 1 + dp[row][col - arr[row]] 
+            dp[row][col] = min(taken, notTaken) 
+    
+    return dp[len(arr) - 1][target] 
+
+
+
+
