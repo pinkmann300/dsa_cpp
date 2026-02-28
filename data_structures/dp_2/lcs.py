b15a7f99f6f04528464667fd54d0005efbc2bb3e
Sandeep Chezhian A <sandeepchezhian@gmail.com>
2026-02-28 22:37:52 +0530

    Completed longest common subsequence using tabulation

--- data_structures/dp_2/lcs.py
+++ data_structures/dp_2/lcs.py
@@ -12,3 +12,22 @@ def recLongestCommonSubsequence(s1, s2, ind1, ind2):
             return max(ind1move, ind2move) 
          
 
+def longestCommonSubsequenceTabulated(s1, s2): 
+    dp = [[0 for _ in range(len(s2) + 1)] for _ in range(len(s1) + 1)] 
+    for i in range(len(s1) + 1): 
+        dp[i][0] = 0 
+    
+    for k in range(len(s2) + 1): 
+        dp[0][k] = 0
+
+    for index1 in range(1, len(s1) + 1): 
+        for index2 in range(1, len(s2) + 1): 
+            if s1[index1 - 1] == s2[index2 - 1]: 
+                dp[index1][index2] = 1 + dp[index1 - 1][index2 - 1] 
+            else: 
+                ind1move = dp[index1 - 1][index2] 
+                ind2move = dp[index1][index2 - 1] 
+
+                dp[index1][index2] = max(ind1move, ind2move) 
+    
+    return dp[len(s1)][len(s2)] 
