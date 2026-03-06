ac46fb464d92cff4ed6fe85f4279b421d770cd9d
Sandeep Chezhian A <sandeepchezhian@gmail.com>
2026-03-06 12:38:07 +0530

    Added second largest element function

--- data_structures/arrays_redone/largestElem.py
+++ data_structures/arrays_redone/largestElem.py
@@ -4,3 +4,17 @@ def largestElement(arr):
         maxVal = max(maxVal, i) 
     
     return maxVal
+
+def secondLargestElement(arr): 
+    largestElem = arr[0] 
+    secondLargestElem = float('-inf') 
+
+    for i in range(1, len(arr)): 
+        if arr[i] > largestElem: 
+            secondLargestElem = largestElem 
+            largestElem = arr[i] 
+        
+        elif (arr[i] > secondLargestElem) and (arr[i] != largestElem) and (arr[i] < largestElem): 
+            secondLargestElem = arr[i] 
+
+    return secondLargestElem 
