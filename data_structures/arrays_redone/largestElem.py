b18670275aaab6cdda2970308132fa3862cd6cb5
Sandeep Chezhian A <sandeepchezhian@gmail.com>
2026-03-07 20:36:56 +0530

    Completed union of 2 arrays

--- data_structures/arrays_redone/largestElem.py
+++ data_structures/arrays_redone/largestElem.py
@@ -25,3 +25,27 @@ def linearSearch(k, nums):
             return i 
         
     return -1  
+
+def unionOfTwoSortedArrays(nums1, nums2): 
+    len1 = len(nums1) 
+    len2 = len(nums2) 
+
+    num1Index, num2Index = 0, 0 
+    unionArray = [] 
+
+    while num1Index < len1 and num2Index < len2: 
+        if nums1[num1Index] > num2Index[num2Index] and unionArray and unionArray[-1] != nums2[num2Index]: 
+            unionArray.append(nums2[num2Index]) 
+            num2Index += 1 
+        
+        elif nums1[num1Index] < num2Index[num2Index] and unionArray and unionArray[-1] != nums1[num1Index]:
+            unionArray.append(nums1[num1Index]) 
+            num1Index += 1
+
+    return unionArray
+
+
+
+
+arr1 = [1,2,3,4,5]  
+arr2 = [2,3,4,4,5]
