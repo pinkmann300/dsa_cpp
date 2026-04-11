eb8111383713673406b99559391381816fba5135
Sandeep Chezhian A <sandeepchezhian@gmail.com>
2026-04-11 21:43:28 +0530

    Count inversions

--- data_structures/arrays_redone/countInversions.py
+++ data_structures/arrays_redone/countInversions.py
@@ -7,3 +7,19 @@ def countInversions(nums):
             if nums[i] > nums[j]:
                 count += 1
     return count 
+
+def mergeSort(nums, low, high): 
+    cnt = 0 
+
+    if low >= high: 
+        return cnt 
+    
+    mid = (low + high) // 2 
+
+    cnt += mergeSort(nums, low, mid) 
+    cnt += mergeSort(nums, mid + 1, high) 
+    cnt += merge(nums, low, mid, high)
+    return cnt 
+
+def merge(nums, l, m, h): 
+    pass 
