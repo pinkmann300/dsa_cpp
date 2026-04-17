d8722c3d0a085fac3a6680825f72de1abe27db62
Sandeep Chezhian A <sandeepchezhian@gmail.com>
2026-04-17 19:25:02 +0530

    Completed enums exercise

--- lld/enums_practice.py
+++ lld/enums_practice.py
@@ -5,8 +5,42 @@ class TrafficLight(Enum):
     YELLOW = 5
     GREEN = 25
 
+    def __init__(self, duration): 
+        self.duration = duration
+
     def next(self): 
-        pass 
+        if self == TrafficLight.RED: 
+            self = TrafficLight.GREEN 
+        elif self == TrafficLight.GREEN: 
+            self = TrafficLight.YELLOW 
+        elif self == TrafficLight.YELLOW: 
+            self = TrafficLight.RED
 
 
 
+class HttpStatusCode(Enum): 
+    OK = (200, "OK") 
+    BAD_REQUEST = (400, "Bad Request") 
+    NOT_FOUND = (404, "Not Found") 
+    INTERNAL_SERVER_ERROR = (500, "Internal Server Error") 
+
+    def __init__(self, code, message): 
+        self.code = code 
+        self.message = message 
+
+    def is_success(self): 
+        if self.code < 400: 
+            return True 
+        else:
+            return False 
+    
+    def display(self): 
+        print(f"{self.code} {self.message}")  
+
+    @staticmethod
+    def from_code(code): 
+        for k in HttpStatusCode: 
+            if k.code == code: 
+                return k  
+
+        return None
