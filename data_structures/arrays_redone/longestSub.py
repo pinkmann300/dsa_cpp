725c2deb758ad4b02185f566fd31702900bbc05e
Sandeep Chezhian A <sandeepchezhian@gmail.com>
2026-04-18 18:51:47 +0530

    Completed longest repeating character replacement problem

--- data_structures/arrays_redone/longestSub.py
+++ data_structures/arrays_redone/longestSub.py
@@ -13,3 +13,22 @@ def countSubarrays(nums, k):
         prefixMap[prefix] = prefixMap.get(prefix, 0) + 1
 
     return count 
+
+
+#Longest repeating character replacement 
+
+def lrcr(stringVal, k): 
+    longestSubStringLength = 0 
+    for i in range(len(stringVal)): 
+        hashArray = [0 for _ in range(26)] 
+        maxFrequency = 0
+
+        for j in range(i, len(stringVal)): 
+            hashArray[ord(stringVal[j]) - ord('A')] += 1 
+            maxFrequency = max(maxFrequency, hashArray[stringVal[j] - 'A'])
+            changes = (j - i + 1) - maxFrequency 
+            if changes <= k: 
+                longestSubStringLength = max(longestSubStringLength, j - i + 1) 
+            else: 
+                break 
+            
